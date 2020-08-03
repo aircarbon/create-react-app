e021088e2392c2122e0bcc2b291d7cdf300ddeab
Huynh Duc Dung <dung@aircarbon.co>
2020-08-03 10:54:19 +0800

    feat(react-scripts): support APP_PREFIX from env

--- packages/react-scripts/config/env.js
+++ packages/react-scripts/config/env.js
@@ -64,13 +64,14 @@ process.env.NODE_PATH = (process.env.NODE_PATH || '')
   .map(folder => path.resolve(appDirectory, folder))
   .join(path.delimiter);
 
-// Grab NODE_ENV and REACT_APP_* environment variables and prepare them to be
+// Grab NODE_ENV and APP_PREFIX_* environment variables and prepare them to be
 // injected into the application via DefinePlugin in webpack configuration.
-const AC = /^AC_/i;
+const prefix = process.env.APP_PREFIX || 'AC';
+const APP_PREFIX = new RegExp(`^${prefix}_`, 'i');
 
 function getClientEnvironment(publicUrl) {
   const raw = Object.keys(process.env)
-    .filter(key => AC.test(key))
+    .filter(key => APP_PREFIX.test(key))
     .reduce(
       (env, key) => {
         env[key] = process.env[key];
